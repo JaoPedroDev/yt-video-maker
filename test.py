3e56a872582291f328f61c23bda32d69933fa702
João Pedro Souza da Cruz <72478575+JaoPedroDev@users.noreply.github.com>
2021-08-20 16:43:09 -0300

    Delete test.py
    Useless file

--- test.py
+++
@@ -1,9 +0,0 @@
-import moviepy.editor as mpy
-import gizeh_title as gt
-
-clip = mpy.VideoFileClip("current_videos/03_Nidhogg 2 Launch Trailer _ PS4.mp4")
-title = "Naruto Ultimate Ninja Storm"
-
-final_clip = gt.title_maker(title, clip)
-
-final_clip.write_gif("teste-git.gif", fps=2)
